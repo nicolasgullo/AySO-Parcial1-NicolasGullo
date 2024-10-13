f258d9b8eed2714b2d2334c81d15c2cdcbcb8509
nicolasgullo <nicolas_gullo@hotmail.com>
2024-10-13 22:59:43 +0000

    agregue punto a y punto b

---
+++ parcial1/puntoa.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+	mkdir -p Examenes-UTN/{{alumno1,alumno2,alumno3}/parcial_{1..3},profesores}
---
+++ parcial1/puntob.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+	lsblk
+
+	echo "Creando particiones..."
+	(
+	echo n 
+	echo p 
+	echo 1 
+	echo   
+	echo +2.5G 
+
+	echo n 
+	echo p 
+	echo 2 
+	echo   
+	echo +2.5G 
+
+	echo n 
+	echo p 
+	echo 3 
+	echo   
+	echo +2.5G 
+
+	echo n 
+	echo e 
+	echo 4 
+	echo   
+	echo
+
+	echo n
+	echo p
+	echo
+	echo
+	echo	
+
+	echo w 
+	) | sudo fdisk /dev/sdc
+
+	echo "Formateando particiones..."
+	sudo mkfs.ext3 /dev/sdc1
+	sudo mkfs.ext3 /dev/sdc2
+	sudo mkfs.ext3 /dev/sdc3
+	sudo mkfs.ext3 /dev/sdc5
+
+	echo "Montando particiones..."
+	sudo mount /dev/sdc1 /mnt
+	sudo mount /dev/sdc2 /mnt
+	sudo mount /dev/sdc3 /mnt
+	sudo mount /dev/sdc5 /mnt
+
+	echo "Particiones montadas:"
+	lsblk
+
